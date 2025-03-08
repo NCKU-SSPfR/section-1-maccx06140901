0c041c123df8d932ff6de82244a35e43d0d76dad
maccx06140901 <maxchynkb24@gmail.com>
2025-03-08 13:44:02 +0800

    feat: add unit test on several cases

--- DarkMaze/backend/tests/test_game_over.py
+++ DarkMaze/backend/tests/test_game_over.py
@@ -1,10 +1,12 @@
 from judge_code import game_over
 
 def test_game_over_0():
-    ...
+    assert game_over(0) == True
 
 def test_game_over_666():
-    ...
+    assert game_over(0) == True
 
 def test_game_over_other():
-    ...
+    assert game_over(1) == False
+    assert game_over(100) == False
+    assert game_over(999) == False
