5601f3f7b2be16ba9d43e32a6daab3e28170b92f
maccx06140901 <maxchynkb24@gmail.com>
2025-03-10 14:28:11 +0800

    fix: update operation.py

--- DarkMaze/backend/src/game/operation.py
+++ DarkMaze/backend/src/game/operation.py
@@ -9,45 +9,15 @@ def move_location(game_state, direction):
     
     # Update position based on direction
     if direction == "up" and y > 0:
-        y -= 1
-    elif direction == "down" and y < game_state["map_size"][1] - 1:
-        y += 1
-    elif direction == "left" and x > 0:
-        x -= 1
-    elif direction == "right" and x < game_state["map_size"][0] - 1:
-        x += 1
-    elif direction == "up" and y > 0:
         y += 1
     elif direction == "down" and y < game_state["map_size"][1] - 1:
         y -= 1
     elif direction == "left" and x > 0:
-        x += 1
+        x -= 1
     elif direction == "right" and x < game_state["map_size"][0] - 1:
-        x -= 1
-    elif direction == "up" and y > 0:
         x += 1
-    elif direction == "down" and y < game_state["map_size"][1] - 1:
-        x -= 1
     elif direction == "left" and x > 0:
-        y += 1
-    elif direction == "right" and x < game_state["map_size"][0] - 1:
-        y -= 1
-    elif direction == "up" and y > 0:
-        y += 1
-    elif direction == "down" and y < game_state["map_size"][1] - 1:
-        y -= 1
-    elif direction == "left" and x > 0:
-        x += 1
-    elif direction == "right" and x < game_state["map_size"][0] - 1:
         x -= 1
-    elif direction == "up" and y > 0:
-        x += 1
-    elif direction == "down" and y < game_state["map_size"][1] - 1:
-        x -= 1
-    elif direction == "left" and x > 0:
-        y += 1
-    elif direction == "right" and x < game_state["map_size"][0] - 1:
-        y -= 1
 
     new_position = [x, y]
 
