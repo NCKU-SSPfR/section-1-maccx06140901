e980754f20b18114f5037ccc234852c47cabb4fd
maccx <maxchynkb24@gmail.com>
2025-03-11 19:39:48 +0800

    fix: rebase all commitment

--- DarkMaze/backend/tests/test_solve_maze.py
+++ DarkMaze/backend/tests/test_solve_maze.py
@@ -56,11 +56,7 @@ async def test_solver():
     await reset_request()
     for i in range(5):
         await move_request("down")
-<<<<<<< HEAD
         await move_request("right")
-=======
-    await move_request("right")
->>>>>>> dc8d0f5 (feat: solve the maze)
     await move_request("down")
     await move_request("right")
     await move_request("right")
